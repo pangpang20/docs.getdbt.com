1b526ebc5b567f3de8c4eb931e3871a3b38ecfc0
Matt Shaver <60105315+matthewshaver@users.noreply.github.com>
2024-11-04 20:16:09 -0500

    Spelling fix

--- website/dbt-versions.js
+++ website/dbt-versions.js
@@ -72,7 +72,7 @@ exports.versionedPages = [
  * }>}
  * @property {string} category The target category to hide/show in the sidebar
  * @property {string} firstVersion The first version the category is visible in the sidebar
- * Testing
+ * Test
  */
 exports.versionedCategories = [
   {
